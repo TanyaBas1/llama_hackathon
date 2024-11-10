65e0971c6d46fc0de47ce2e1a4d0d0c9743807bd
Tetiana Bas <tetiana@uni.minerva.edu>
2024-11-09 16:17:50 -0800

    completed setup

--- README.md
+++ README.md
@@ -1,9 +1,38 @@
+## Set up 
+
 To run this you will have to go into the poetry shell 
 
-'''bash
+```bash
 poetry shell
-'''
+```
 
-'''bash
+```bash
 poetry install
-'''
+```
+
+after that you have to load environmental variables 
+- create .env
+- copy the content of .env.example 
+- instert the api key (feel free to message me to ask it)
+
+
+to run a module 
+
+```bash
+python path/to/your/module
+```
+
+## Useful debugging commands 
+
+to check if the api keys are loaded 
+
+```bash
+poetry run echo $TOGETHER_API_KEY
+```
+
+## Important notes
+
+Please create a branch with your name and work on a branch, do not push into main
+also try to add comments about what you do and what are the next steps that should be done - it will make the handover easier
+
+Thank you!
--- llm_agents/llm_utils.py
+++ llm_agents/llm_utils.py
@@ -1,1 +1,40 @@
 "this modele contains the helper functions for LLM agents"
+from together import Together
+from dotenv import load_dotenv
+import os
+
+
+
+def call_togather(client,model, system_prompt, user_prompt):
+    response = client.chat.completions.create(
+        model=model,
+        messages=[
+            {"role": "system", "content": system_prompt},
+            {"role": "user", "content": [{"type": "text", "text": user_prompt}]}],
+        max_tokens=None,
+        temperature=0.7,
+        top_p=0.7,
+        top_k=50,
+        repetition_penalty=1,
+        stop=["<|eot_id|>","<|eom_id|>"],
+        stream=True
+    )
+    # Accumulate the response content instead of printing it
+    response_content = ''
+    for token in response:
+        if hasattr(token, 'choices') and token.choices and hasattr(token.choices[0], 'delta'):
+            response_content += token.choices[0].delta.content
+
+    # TODO 
+    return response_content
+
+
+if __name__ == "__main__":
+    load_dotenv()
+    api_key = os.getenv("TOGETHER_API_KEY")
+    client = Together(api_key=api_key)
+    model = "meta-llama/Llama-3.2-11B-Vision-Instruct-Turbo"
+    system_prompt = "You are a llama farmer and you are trying to figure out how to increase your profits. You have a llama named Carl who is very smart and can help you with your business. You ask Carl for advice on how to increase your profits."
+    user_prompt = "Carl, how can I increase my profits?"
+    response = call_togather(client,model, system_prompt, user_prompt)
+    print(response)
